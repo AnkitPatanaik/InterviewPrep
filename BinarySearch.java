426b07440cf0cd9b806b394ced54aca932893fd4
AnkitPatanaik <ankit@purdue.edu>
2016-10-31 23:46:18 -0400

    i think it works

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,27 +4,21 @@ public class BinarySearch {
 	public static boolean binarySearch(int[] array, int k) {
 		int low = 0;
 		int high = array.length - 1;
-		int median = array[(low+high)/2];
-		System.out.println("MEDIAN " + median);
 
-		while (low != high) {
-			if (k > median) {
-				low = high - ((low + high)/2) - 1;
+		while (low <= high) {
+			int median = low + (high - low)/2;
+			if (k > array[median]) {
+				low = median + 1;
 				//System.out.println("Reset low: " + low);
-				//return false;
 			}
-			else if (k < median) {
-				high = low + ((low + high)/2) + 1;
+			else if (k < array[median]) {
+				high = median - 1;
 				//System.out.println("Reset high: " + high);
-				//return false;
 			}
 			else {
 				System.out.println("You found " + k);
 				return true;
 			}
-			median = array[(low+high)/2];
-			//System.out.println(median);
-			//return false;
 		}
 		System.out.println("Your life sucks, you're either unlucky af or your algorithm sucks");
 		return false;
@@ -34,8 +28,7 @@ public class BinarySearch {
 		int[] array1 = {1,2,3,4,5,7,8,9,10};
 		int[] array2 = {2,16,177,1299,2612,3562,50000, 55000, 64622};
 		int[] small = {1, 4, 6};
-
-		//System.out.println(array1.length + " " + array2.length);
+		
 		binarySearch(small, 4);
 		binarySearch(small, 5);
 		binarySearch(array1, 7);
