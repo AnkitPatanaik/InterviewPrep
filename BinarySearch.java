17ca7b7e672b3c57263916315b701396a237fb27
AnkitPatanaik <ankit@purdue.edu>
2016-10-31 22:39:27 -0400

    Fixed my array syntax because i'm stupid

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,7 @@
 public class BinarySearch {
 	public static void main(String[] args) {
-		int[] array1 = [1,2,3,4,5,7,8,9,10];
-		int[] array2 = [2,16,177,1299,2612,3562,64622];
+		int[] array1 = {1,2,3,4,5,7,8,9,10};
+		int[] array2 = {2,16,177,1299,2612,3562,64622};
+		System.out.println(array1.length + " " + array2.length);
 	}
 }
