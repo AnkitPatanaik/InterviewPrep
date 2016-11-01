7a3dc96dfc725531ae74752b2db9c0cbaa6f937d
AnkitPatanaik <ankit@purdue.edu>
2016-10-31 23:54:26 -0400

    Added more tests, commented and cleaned

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,19 +1,20 @@
 public class BinarySearch {
 
 	/* Takes a sorted array and returns true if the value k is found */
+	/* O(logn) time */
 	public static boolean binarySearch(int[] array, int k) {
+		/* initialize low and high indices */
 		int low = 0;
 		int high = array.length - 1;
 
 		while (low <= high) {
-			int median = low + (high - low)/2;
+			int median = low + (high - low)/2; //calculate the midpoint index
+
 			if (k > array[median]) {
 				low = median + 1;
-				//System.out.println("Reset low: " + low);
 			}
 			else if (k < array[median]) {
 				high = median - 1;
-				//System.out.println("Reset high: " + high);
 			}
 			else {
 				System.out.println("You found " + k);
@@ -28,9 +29,20 @@ public class BinarySearch {
 		int[] array1 = {1,2,3,4,5,7,8,9,10};
 		int[] array2 = {2,16,177,1299,2612,3562,50000, 55000, 64622};
 		int[] small = {1, 4, 6};
-		
-		binarySearch(small, 4);
-		binarySearch(small, 5);
-		binarySearch(array1, 7);
+		int[] big = {1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,45,47,48,58,69,70,100,123,124,125,126,7372,31413,50000,372372,4723241};
+
+		binarySearch(small, 4);  /* True */
+		binarySearch(small, 5); /* False */
+		binarySearch(array1, 7);  /* True */
+		binarySearch(array1, 15); /* False */
+		binarySearch(array2, 15); /* False */
+		binarySearch(array2, 12512); /* False */
+		binarySearch(array2, 200); /* False */
+		binarySearch(array2, 3562);  /* True */
+		binarySearch(big, 101); /* False */
+		binarySearch(big, 101000); /* False */
+		binarySearch(big, 1000); /* False */
+		binarySearch(big, 7372);  /* True */
+		binarySearch(big, 1);  /* True */
 	}
 }
