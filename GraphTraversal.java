a49f6c6865b2133a729dd68c589749dd2d40262d
AnkitPatanaik <ankit@purdue.edu>
2016-11-02 10:52:45 -0400

    lowkey works, need to fix formatting

--- GraphTraversal.java
+++ GraphTraversal.java
@@ -7,17 +7,33 @@ public class GraphTraversal {
 		//for each child, add to queue
 		//print children
 	
-	public static void bfs (BST<Integer> tree) {
+	public static void bfs (Node root) {
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(root);
 
+		while (!q.isEmpty()) {
+			Node a = q.poll();
+			System.out.print(a.data + " ");
+			if (a.left != null) {
+				q.add(a.left);
+				
+			}
+			if (a.right != null) {
+				q.add(a.right);
+				
+			}
+
+			System.out.println();
+		}
 
 	}
 
 	public static void main (String[] args) {
-		Integer[] a = {1,5,2,7,4};
-      	Node bst = new Node(0);
+		Integer[] a = {1,0,2,10,7,6,11,25,9,14};
+      	Node bst = new Node(15);
       	for(Integer n : a) bst.insert(bst, n);
-      	bst.inOrderTraversal(bst);
+      	//bst.inOrderTraversal(bst);
 
-      	//bfs(bst);
+      	bfs(bst);
 	}
 }
