8b5d3967439d8bc8313fb424b76f7da32f8928ea
AnkitPatanaik <ankit@purdue.edu>
2016-11-08 11:27:50 -0500

    need to fix reset of list

--- Subsequence.java
+++ Subsequence.java
@@ -2,27 +2,43 @@ import java.util.*;
 
 public class Subsequence {
 	
+	/* Return the longest increasing subsequence in an array */
 	public static int[] longestIncreasing(int[] array) {
 		int min = Integer.MIN_VALUE;
 		int[] temp = new int[array.length];
 		int index = 0;
+		ArrayList<int[]> list = new ArrayList<int[]>();
 
 		for (int i = 0; i < array.length; i++) {
 			int a = array[i];
-			if (a > min) {
+			if (a > min) { //add to array
 				temp[index++] = a;
 				min = a;
 			}
+			else {
+				//reset min
+				min = Integer.MIN_VALUE;
+				list.add(temp);
+				//reset temp array;
+				temp = new int[array.length];
+				index = 0;
+			}
 		}
-		return temp;
+		return findLongest(list);
+	}
+
+	public static int[] findLongest(ArrayList<int[]> list) {
+		return null;
 	}
 
 	public static void printArray(int[] a) {
 		System.out.println("--------");
 		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i]);
+			System.out.print(a[i] + " ");
 		}
+		System.out.println();
 		System.out.println("--------");
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
@@ -32,10 +48,11 @@ public class Subsequence {
 		int[] test4 = {1,2,3}; /* 1,2,3 */
 		int[] test5 = {1,2,3,-100,5,6,7,8,9}; /* 5,6,7,8,9 */
 
-		// printArray(longestIncreasing(test4));
-		// printArray(longestIncreasing(test1));
-		// printArray(longestIncreasing(test2));
-		// printArray(longestIncreasing(test3));
+		
+		printArray(longestIncreasing(test1));
+		printArray(longestIncreasing(test2));
+		printArray(longestIncreasing(test3));
+		printArray(longestIncreasing(test4));
 		printArray(longestIncreasing(test5));
 	}
 }
