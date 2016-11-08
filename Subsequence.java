399e40fea910d1fb8e75bc943c5f5330555fff84
AnkitPatanaik <ankit@purdue.edu>
2016-11-08 10:54:39 -0500

    Subsequence Class for Subsequence Problems

---
+++ Subsequence.java
@@ -0,0 +1,7 @@
+public class Subsequence {
+	public static void main(String[] args) {
+		int[] test1 = {1,2,3,4,5,6,-5,-6}; /* 1,2,3,4,5,6 */
+		int[] test2 = {-6,-9,-10,1,2,3,4,5,6}; /* 1,2,3,4,5,6 */
+		int[] test1 = {5,1,6,2,7,3,8,6,10}; /* return 1 element */
+	}
+}
