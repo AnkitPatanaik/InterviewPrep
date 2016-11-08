6fa86782ad98952d352338e448ed5eb1e44678a4
AnkitPatanaik <ankit@purdue.edu>
2016-11-08 10:55:12 -0500

    Longest Increasing Subsequence - initial commit

--- Subsequence.java
+++ Subsequence.java
@@ -1,4 +1,11 @@
+import java.util.*;
+
 public class Subsequence {
+	
+	public static int[] longestIncreasing(int[] array) {
+		return null;
+	}
+
 	public static void main(String[] args) {
 		int[] test1 = {1,2,3,4,5,6,-5,-6}; /* 1,2,3,4,5,6 */
 		int[] test2 = {-6,-9,-10,1,2,3,4,5,6}; /* 1,2,3,4,5,6 */
